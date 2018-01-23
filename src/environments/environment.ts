c243398f025078b3a01ca86a8cac190fa4638970
Freddy Manrique <fredar21@gmail.com>
2018-01-23 13:32:24 -0500

    add angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBbPiE7ENAeCZomxX1xwesKvxQ7vdtcHTA",
+    authDomain: "chart-e8c32.firebaseapp.com",
+    databaseURL: "https://chart-e8c32.firebaseio.com",
+    projectId: "chart-e8c32",
+    storageBucket: "chart-e8c32.appspot.com",
+    messagingSenderId: "93279140633"
+  }
 };
